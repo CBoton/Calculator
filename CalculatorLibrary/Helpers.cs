575adbdb612fc062cdd2653072f4e240fdaee38b
Curti <curtisbotonis@gmail.com>
2024-07-04 16:34:41 -0700

    Adding List of previous problems

---
+++ CalculatorLibrary/Helpers.cs
@@ -0,0 +1,32 @@
+﻿using CalculatorLibrary.Models;
+using System.Security.AccessControl;
+
+namespace CalculatorLibrary
+{
+    public class Helpers
+    {
+        internal static List<Problem> problems = new List<Problem>();
+
+        internal static void PrintProblems()
+        { 
+            Console.Clear();
+            Console.WriteLine("Previous Problems");
+            Console.WriteLine("---------------------\n");
+            foreach (var problem in problems)
+            {
+                Console.WriteLine("$ {problem.FirstNumber} {problem.Operand} {problem.SecondNumber} = {problem.Answer}");
+            }
+        }
+
+        internal static void AddToProblems(double firstNumber, char operand, double secondNumber, double answer)
+        {
+            problems.Add(new Problem
+            {
+                FirstNumber = firstNumber,
+                Operand = operand,
+                SecondNumber = secondNumber,
+                Answer = answer
+            });
+        }
+    }
+}
---
+++ CalculatorLibrary/Models/Problem.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorLibrary.Models
+{
+    internal class Problem
+    {
+        public double FirstNumber { get; set; }
+
+        public double SecondNumber { get; set; }
+
+        public char Operand { get; set; }
+
+        public double Answer { get; set; }
+    }
+
+    
+}
